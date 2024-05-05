d481dcf59e0cce8ecf02c449e5f26832cf1abbde
Arnob <118902649+ArnobTurja2002Ghosh@users.noreply.github.com>
2024-05-05 17:14:31 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,55 +3,140 @@
 
 #include "SFML/Graphics.hpp"
 #include <iostream>
+#include <fstream>
+
+class AShape {       // The class
+public:             // Access specifier
+    sf::CircleShape circle;        // Attribute (int variable)
+    sf::RectangleShape rectangle;  // Attribute (string variable)
+    float speedX,speedY;
+    sf::Text name;
+    AShape() {
+    }
+};
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Window Title");
-    window.setFramerateLimit(60);
+    sf::RenderWindow window;
+    sf::Font font;
 
+    std::ifstream fin("config.txt");
+    std::string temp;
+    int characterSize, textR, textG, textB;
+    std::vector<AShape> circles;
+
+    while (fin >> temp) {
+        if (temp.compare("Window") == 0) {
+            int windowWidth, windowHeight;
+            fin >> windowWidth >> windowHeight;
+            window.create(sf::VideoMode(windowWidth, windowHeight), "Window Title"); 
+        }
+        else if (temp.compare("Font") == 0) {
+            std::string font1;
+            
+            fin >> font1>> characterSize>>textR>>textG>>textB;
+            if (!font.loadFromFile(font1))
+            {
+                std::cout << "Download Arial!\n";
+            }
+        }
+        else if (temp.compare("Circle") == 0) {
+            AShape circle1;
+
+            fin >> temp;
+            circle1.name.setFont(font); // font is a sf::Font
+            circle1.name.setString(temp);
+            circle1.name.setCharacterSize(characterSize); // in pixels, not points!
+            circle1.name.setFillColor(sf::Color(textR, textG, textB));
+            circle1.name.setStyle(sf::Text::Bold | sf::Text::Underlined);
+            circle1.name.setOrigin(circle1.name.getLocalBounds().width / 2.f + circle1.name.getLocalBounds().left, circle1.name.getLocalBounds().height / 2.f + circle1.name.getLocalBounds().top);
+            
+            int temp1, temp2;
+            fin >> temp1 >> temp2;
+            circle1.circle.setPosition(temp1, temp2); // Center circle
+            std::cout << temp1 << temp2;
+            fin >> temp1 >> temp2;
+            circle1.speedX = temp1;
+            circle1.speedY = temp2;
+
+            int temp3;
+            fin >> temp1 >> temp2>>temp3;
+            circle1.circle.setFillColor(sf::Color
+            (
+                (int)(temp1),
+                (int)(temp2),
+                (int)(temp3)
+            ));
+
+
+            fin >> temp3;
+            circle1.circle.setRadius(temp3);
+            circle1.circle.setOrigin(temp3, temp3);
+            
+            
+            // select the font
+            
+
+            circles.push_back(circle1);
+        }
+        else if (temp.compare("Rectangle") == 0) {
+            AShape circle1;
+
+            fin >> temp;
+            circle1.name.setFont(font); // font is a sf::Font
+            circle1.name.setString(temp);
+            circle1.name.setCharacterSize(characterSize); // in pixels, not points!
+            circle1.name.setFillColor(sf::Color(textR, textG, textB));
+            circle1.name.setStyle(sf::Text::Bold | sf::Text::Underlined);
+            circle1.name.setOrigin(circle1.name.getLocalBounds().width / 2.f + circle1.name.getLocalBounds().left, circle1.name.getLocalBounds().height / 2.f + circle1.name.getLocalBounds().top);
+
+            int temp1, temp2;
+            fin >> temp1 >> temp2;
+            circle1.rectangle.setPosition(temp1, temp2); // Center circle
+            std::cout << temp1 << temp2;
+            fin >> temp1 >> temp2;
+            circle1.speedX = temp1;
+            circle1.speedY = temp2;
+
+            int temp3;
+            fin >> temp1 >> temp2 >> temp3;
+            circle1.rectangle.setFillColor(sf::Color
+            (
+                (int)(temp1),
+                (int)(temp2),
+                (int)(temp3)
+            ));
+
+
+            fin >> temp1>>temp2;
+            circle1.rectangle.setSize(sf::Vector2f(temp1, temp2));
+            circle1.rectangle.setOrigin(temp1/2, temp2/2);
+
+
+            // select the font
+
+
+            circles.push_back(circle1);
+        }
+    }
+    
+    window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
-    sf::Font font;
-    if (!font.loadFromFile("arial.ttf"))
-    {
-        std::cout << "Download Arial!\n";
-    }
-
-    sf::Text text;
-
-    // select the font
-    text.setFont(font); // font is a sf::Font
-
-    // set the string to display
-    text.setString("CGreen");
-
-    // set the character size
-    text.setCharacterSize(18); // in pixels, not points!
-
-    // set the color
-    text.setFillColor(sf::Color::White);
-
-    // set the text style
-    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-    text.setOrigin(text.getLocalBounds().width / 2.f + text.getLocalBounds().left, text.getLocalBounds().height / 2.f + text.getLocalBounds().top);
     
+    
+       
 
     bool circleExists = true;
     float circleRadius = 50.0f;
     int circleSegments = 100;
     float circleColor[3] = { (float)0 / 255, (float)255 / 255, (float)0 / 255 };
-    float circleSpeedX = -3.0f;
-    float circleSpeedY = 2.0f;
-    sf::CircleShape shape(circleRadius, circleSegments);
-    shape.setFillColor(sf::Color
-    (
-        (int)(circleColor[0] * 255),
-        (int)(circleColor[1] * 255),
-        (int)(circleColor[2] * 255)
-    )); // Color circle
-    shape.setOrigin(circleRadius, circleRadius);
-    shape.setPosition(100, 100); // Center circle
-    text.setPosition(shape.getPosition());
+
+    
+    
+   
+    //text.setPosition(shape.getPosition());
     char displayString[255] = "CGreen";
     sf::Clock deltaClock;
     ImGui::GetStyle().ScaleAllSizes(1.0f);
@@ -76,33 +161,52 @@ int main()
         
         ImGui::InputText("Text", displayString, 255);
         if (ImGui::Button("Set Text")) {
-            text.setString(displayString);
+            //text.setString(displayString);
         }
         ImGui::End();
 
-        shape.setRadius(circleRadius);
-        shape.setOrigin(circleRadius, circleRadius);
-        shape.setPosition(shape.getPosition().x + circleSpeedX, shape.getPosition().y + circleSpeedY);
-        text.setPosition(shape.getPosition().x, shape.getPosition().y);
-        if (shape.getPosition().x + circleRadius >= window.getSize().x || shape.getPosition().x - circleRadius <= 0) {
-            circleSpeedX *= -1;
+        for (auto& c : circles) {
+            //c.circle.setRadius(circleRadius);
+            //c.circle.setOrigin(circleRadius, circleRadius);
+            std::string name_string = c.name.getString();
+            if (name_string[0] == 'C') {
+            
+                c.circle.setPosition(c.circle.getPosition().x + c.speedX, c.circle.getPosition().y + c.speedY);
+                std::cout << c.circle.getPosition().x << c.circle.getPosition().y << "\n";
+                c.name.setPosition(c.circle.getPosition());
+
+                if (c.circle.getPosition().x + c.circle.getRadius() >= window.getSize().x || c.circle.getPosition().x - c.circle.getRadius() <= 0) {
+                    c.speedX *= -1;
+                }
+                if (c.circle.getPosition().y + c.circle.getRadius() >= window.getSize().y || c.circle.getPosition().y - c.circle.getRadius() <= 0) {
+                    c.speedY *= -1;
+                }
+            }
+            else if (name_string[0] == 'R') {
+                c.rectangle.setPosition(c.rectangle.getPosition().x + c.speedX, c.rectangle.getPosition().y + c.speedY);
+                c.name.setPosition(c.rectangle.getPosition());
+                if (c.rectangle.getPosition().x + c.rectangle.getSize().x/2 >= window.getSize().x || c.rectangle.getPosition().x - c.rectangle.getSize().x / 2 <= 0) {
+                    c.speedX *= -1;
+                }
+                if (c.rectangle.getPosition().y + c.rectangle.getSize().y / 2 >= window.getSize().y || c.rectangle.getPosition().y - c.rectangle.getSize().y / 2 <= 0) {
+                    c.speedY *= -1;
+                }
+            }
         }
-        if (shape.getPosition().y + circleRadius >= window.getSize().y || shape.getPosition().y - circleRadius <= 0) {
-            circleSpeedY *= -1;
-        }
-        shape.setPointCount(circleSegments);
-        shape.setFillColor(sf::Color
-        (
-            (int)(circleColor[0] * 255),
-            (int)(circleColor[1] * 255),
-            (int)(circleColor[2] * 255)
-        )); // Color circle
+
+        
+        //text.setPosition(shape.getPosition().x, shape.getPosition().y);
+        
 
         window.clear(sf::Color(18, 33, 43)); // Color background
         if (circleExists) {
-            window.draw(shape);
-            window.draw(text);
+            //window.draw(text);
         }
+        for (auto a : circles) {
+            window.draw(a.circle);
+            window.draw(a.rectangle);
+            window.draw(a.name);
+        }  
         ImGui::SFML::Render(window);
         window.display();
     }
